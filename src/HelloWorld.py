9648f552449d2576a6634f7965bad75b3dd86d7f
Shane <wuudann@126.com>
2016-09-21 19:00:22 +0800

    bottle insert

--- src/HelloWorld.py
+++ src/HelloWorld.py
@@ -6,12 +6,13 @@ class HelloWorld:
         a = int(input("a:"))
         b = 1
         c = 2
+        [d,e] = {2, 4}
         if (a >= 0):
             disc = math.sqrt(a)
         else:
             disc = "error"
         print(disc*3,end='')
-        print("end")
+        print("end with", [d,e])
 
 
 HelloWorld().demo()
